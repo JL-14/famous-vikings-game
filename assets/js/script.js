489c489ca3376cd874db6b1dc29ac21e787945dd
JL-14 <>
2023-07-17 16:10:18 +0000

    Add start and quit button functionality in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,26 @@
-/** Start button launching game */
+/* Start button launching game */
 let gameDisplay = document.getElementById("game-screen");
 let homeDisplay = document.getElementById("home-screen");
 let startButton = document.getElementById("start-button");
 
-startButton.onclick = function() {
+startButton.onclick = function startGame() {
     if (gameDisplay.style.display === "none") {
         homeDisplay.style.display = "none";
-        gameDisplay.style.display = "block";
+        gameDisplay.style.display = "inline-flex";
+    } else {
+        gameDisplay.style.display = "none";
     }
 }
+console.log(gameDisplay);
+console.log(homeDisplay);
+
+/* Quit button to return to Home Page */
+let quitButton = document.getElementById("quit-button");
+quitButton.onclick = function goHome() {
+    if (homeDisplay.style.display === "none") {
+        gameDisplay.style.display = "none";
+        homeDisplay.style.display = "block";
+    } else {
+        homeDisplay.style.display = "none";
+    }
+};
