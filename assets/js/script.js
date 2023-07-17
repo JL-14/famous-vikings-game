bf08453fbc1c0a9aa993bc9615b535155500eb3e
JL-14 <>
2023-07-17 11:35:21 +0000

    Add initial start button js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
-let startButton = document.getElementsByClassName("start-button");
-addEventListener()
+/** Start button launching game */
+let gameDisplay = document.getElementById("game-screen");
+let homeDisplay = document.getElementById("home-screen");
+let startButton = document.getElementById("start-button");
 
-if start-button () {
-
+startButton.onclick = function() {
+    if (gameDisplay.style.display === "none") {
+        homeDisplay.style.display = "none";
+        gameDisplay.style.display = "block";
+    }
 }
