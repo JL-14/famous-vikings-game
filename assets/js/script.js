163f739f9e9c081940d36ebecf74619cee68dcfb
JL-14 <>
2023-07-18 14:56:17 +0000

    Add code to assign random code to option buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,7 @@
 
 /* Randomise options in question grid */
-let options = [];
 
-function main() {
-    options = ["Became Duke of Normandy", "Discovered America", "Established North Sea Empire", "The Last Great Viking", "Made London Bridge fall down", "Carried a 6 foot axe", "Had a blue tooth", "Conquered St Petersburg", "Invented the compass"];
-    alert(options);
-}
+let options = ["Became Duke of Normandy", "Discovered America", "Established North Sea Empire", "The Last Great Viking", "Made London Bridge fall down", "Carried a 6 foot axe", "Had a blue tooth", "Conquered St Petersburg", "Invented the compass"];
 
 function randomOptions() {
     window.alert("The current order is: " + options);
@@ -20,10 +16,20 @@ function randomOptions() {
         options[i] = shuffledOptions;
         options[randomNum] = tempOptions;
     }
-    window.alert("The shuffled options are: ");
+    window.alert("The shuffled options are: " + options);
+
+    document.querySelector('#button1').textContent = options[0];
+    document.querySelector('#button2').textContent = options[1];
+    document.querySelector('#button3').textContent = options[2];
+    document.querySelector('#button4').textContent = options[3];
+    document.querySelector('#button5').textContent = options[4];
+    document.querySelector('#button6').textContent = options[5];
+    document.querySelector('#button7').textContent = options[6];
+    document.querySelector('#button8').textContent = options[7];
+    document.querySelector('#button9').textContent = options[8];
 }
 
-
+console.log(options[0]);
 
 /* Start button launching game */
 let gameDisplay = document.getElementById("game-screen");
@@ -34,12 +40,11 @@ startButton.onclick = function startGame() {
     if (gameDisplay.style.display === "none") {
         homeDisplay.style.display = "none";
         gameDisplay.style.display = "inline-flex";
+        randomOptions();
     } else {
         gameDisplay.style.display = "none";
     }
 }
-console.log(gameDisplay);
-console.log(homeDisplay);
 
 /* Quit button to return to Home Page */
 let quitButton = document.getElementById("quit-button");
